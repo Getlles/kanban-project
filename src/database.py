652730620cfe6f3494ee570950803ff064793f2e
Getlles <attencrow@gmail.com>
2024-12-18 13:33:39 +0300

    db

--- src/database.py
+++ src/database.py
@@ -25,6 +25,7 @@ def fetch_query(query, params=None): #Для селекта
         print(f"Ошибка выполнения запроса: {e}")
 
 
+
 def init_db():
     cur.execute('''
         CREATE TABLE IF NOT EXISTS users (
